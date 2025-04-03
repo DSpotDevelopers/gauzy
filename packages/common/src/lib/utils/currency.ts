28c843052e22f1855ce2c3cd4b1bfb7e4200460f
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2025-04-03 09:04:16 -0500

    refactor: Show currency codes instead of symbols

--- packages/common/src/lib/utils/currency.ts
+++ packages/common/src/lib/utils/currency.ts
@@ -1,13 +1,7 @@
-import { CURRENCY_SYMBOLS } from '@gauzy/contracts';
-
-export function getCurrencySymbol(currency: string) {
-    return CURRENCY_SYMBOLS[currency] ?? currency;
-}
-
 export function currencyFormat(value: number) {
     return Number(value).toFixed(2);
 }
 
 export function currencyWithSymbol(value: number, currency: string) {
-    return `${getCurrencySymbol(currency)} ${currencyFormat(value)}`;
+    return `${currency} ${currencyFormat(value)}`;
 }
